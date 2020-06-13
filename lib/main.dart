cb2fd79eb9868ed1bcb5aa11a8a0dd5493c74368
José Javier Illesca Fernández <jjavierillesca@gmail.com>
2020-06-13 19:14:27 +0200

    App Table design with BottonNavigatorBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:designsapp/src/pages/basic_page.dart';
 import 'package:designsapp/src/pages/scroll_page.dart';
+import 'package:designsapp/src/pages/buttons_page.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle.light.copyWith(
+        statusBarColor: ( Platform.isAndroid ) ? Colors.transparent : Colors.white
+    ));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'scroll',
+      initialRoute: 'buttons',
       routes: {
         'basic'   : (BuildContext context) => BasicPage(),
         'scroll'  : (BuildContext context) => ScrollPage(),
+        'buttons'  : (BuildContext context) => ButtonsPage(),
       }
     );
   }
